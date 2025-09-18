3c54f7967d878e5cc05b4631af18f4562b452788
tox <toxtek@proton.me>
2025-09-17 22:52:28 -0500

    feat: track more information in commit.sh
    Now collect data on the types of versions updated, and add more accurate
    counts for packages with updates.

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+packages_updated=$(git status -s p | grep '^ M' | grep -Ev 'ALL|delay' | cut -d/ -f2 | uniq | wc -l)
+
+release_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep release | wc -l)
+unstable_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep unstable | wc -l)
+commit_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep commit | wc -l)
+
 desc="
 [$(date +"%Y-%m-%d %H:%M:%S %z")]
 Run #$(<runcount) took $(<elapsed)
@@ -7,12 +13,16 @@ Run #$(<runcount) took $(<elapsed)
 - Checked $(<checked) packages
 - Skipped $(<skipped) packages
 - Failed to fetch versions for $(<failed) packages
-- Updated versions for $(git status -s | grep '^ M ' | grep -o '\w\+/' | tr -d / | uniq | wc -l) packages
+
+- Updated $(($release_versions_updates + $unstable_versions_updates + $commit_versions_updates)) versions for $packages_updated packages:
+    - Release versions:  $release_versions_updates
+    - Unstable versions: $unstable_versions_updates
+    - Commit versions:   $commit_versions_updates
 "
 
 echo "$desc"
 # git add p/*/delay runcount
-# git commit -m "Update internal data"
+# git commit -m "auto(aux): update internal data"
 #
 # git add p/ALL p/*/{release,unstable,commit}
-# git commit -m "Update versions" -m "$desc"
+# git commit -m "auto(p): update versions" -m "$desc"
