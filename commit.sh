47005d1f09868bd8db0b94c0eead21df89b31a8e
Toxikuu <toxtek@proton.me>
2025-10-12 06:17:05 -0500

    !feat(sh): adapt commit.sh to new api

--- commit.sh
+++ commit.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-packages_updated=$(git status -s p | grep '^ M' | grep -Ev 'ALL' | cut -d/ -f2 | uniq | wc -l)
+git add runcount
+git commit -m "auto(aux): update internal data"
 
-release_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep release | wc -l)
-unstable_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep unstable | wc -l)
-commit_versions_updates=$(git status -s p | grep '^ M' | cut -d/ -f2- | uniq | grep commit | wc -l)
+git add p/ALL.* p/*/versions.* p/*/channels/*
+
+packages_updated=$(git status -s p | grep -E '^( M|A |\?\?)' | grep -vF ALL | cut -d/ -f2 | uniq | wc -l)
+versions_updates=$(git status -s p | grep -E '^( M|A |\?\?)' | cut -d/ -f2- | uniq | grep -F channels/)
+
+release_versions_updates=$(echo "$versions_updates" | grep -c 'channels/release$')
+unstable_versions_updates=$(echo "$versions_updates" | grep -c 'channels/unstable$')
+commit_versions_updates=$(echo "$versions_updates" | grep -c 'channels/commit$')
+other_versions_updates=$(echo "$versions_updates" | grep -Evc 'channels/(release|unstable|commit)$')
+
+versions_updates=$(echo "$versions_updates" | wc -l)
 
 desc="
 [$(date +"%Y-%m-%d %H:%M:%S %z")]
@@ -14,15 +23,13 @@ Run #$(<runcount) took $(<elapsed)
 - Skipped $(<skipped) packages
 - Failed to fetch versions for $(<failed) packages
 
-- Updated $(($release_versions_updates + $unstable_versions_updates + $commit_versions_updates)) versions for $packages_updated packages:
+- Updated $((versions_updates)) versions for $packages_updated packages:
     - Release versions:  $release_versions_updates
     - Unstable versions: $unstable_versions_updates
     - Commit versions:   $commit_versions_updates
+    - Other versions:    $other_versions_updates
 "
 
 echo "$desc"
-git add runcount
-git commit -m "auto(aux): update internal data"
 
-git add p/ALL p/*/{release,unstable,commit}
 git commit -m "auto(p): update versions" -m "$desc"
